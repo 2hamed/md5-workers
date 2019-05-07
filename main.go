8a3bbd9c7e56ef8d0deb26b4c4852a28f5deaaba
Hamed Momeni <hamed.ma7@gmail.com>
2019-05-07 10:18:01 +0430

    sync version done

--- main.go
+++ main.go
@@ -12,11 +12,15 @@ import (
 func main() {
 
 	filePath := os.Args[1]
+	output := os.Args[2]
 
 	err := filepath.Walk(filePath, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
-			md5Sum, err := md5File(path)
-			fmt.Println(md5Sum, err)
+			md5Sum, _ := md5File(path)
+			fmt.Println(md5Sum, path)
+			if err := writeToFile(path, md5Sum, output); err != nil {
+				panic(err)
+			}
 		}
 		return nil
 	})
@@ -40,4 +44,18 @@ func md5File(filePath string) (string, error) {
 	checksum := hash.Sum(nil)
 
 	return string(hex.EncodeToString(checksum)), nil
+}
+
+func writeToFile(filePath, md5sum, output string) error {
+
+	file, err := os.OpenFile(output, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	file.WriteString(fmt.Sprintf("%s %s\n", md5sum, filePath))
+
+	return file.Sync()
 }
